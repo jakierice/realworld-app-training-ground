156c17cbe2412cbfbdab970f0b251f45a6128038
Jakie W. Rice <jakie.rice@gmail.com>
2020-12-31 14:08:48 -0600

    feat: integrate tags endpoint into ArticleList UI

--- src/TagList.js
+++ src/TagList.js
@@ -1,33 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { getTags } from './api-client'
+
+export const IDLE = 'idle'
+export const PENDING = 'pending'
+export const SUCCESS = 'success'
+export const FAILURE = 'failure'
+
 export function TagList() {
+  const [requestState, setRequestState] = React.useState(IDLE)
+  const [tags, setTags] = React.useState([])
+
+  React.useEffect(() => {
+    setRequestState(PENDING)
+
+    getTags()
+      .then(setTags)
+      .then(() => void setRequestState(SUCCESS))
+      .catch((_error) => {
+        setRequestState(FAILURE)
+      })
+  }, [])
+
   return (
     <>
       <p>Popular Tags</p>
-
       <div className="tag-list">
-        <a href="" className="tag-pill tag-default">
-          programming
-        </a>
-        <a href="" className="tag-pill tag-default">
-          javascript
-        </a>
-        <a href="" className="tag-pill tag-default">
-          emberjs
-        </a>
-        <a href="" className="tag-pill tag-default">
-          angularjs
-        </a>
-        <a href="" className="tag-pill tag-default">
-          react
-        </a>
-        <a href="" className="tag-pill tag-default">
-          mean
-        </a>
-        <a href="" className="tag-pill tag-default">
-          node
-        </a>
-        <a href="" className="tag-pill tag-default">
-          rails
-        </a>
+        {requestState === PENDING && <span>Loading...</span>}
+        {requestState === FAILURE && <span>Could not load tags.</span>}
+        {requestState === SUCCESS &&
+          tags
+            .filter((tag) => tag.split().every((c) => c.charCodeAt(0) !== 8204))
+            .map((tag) => (
+              <Link to={`/?tag=${tag}`} className="tag-pill tag-default">
+                {tag}
+              </Link>
+            ))}
       </div>
     </>
   )
